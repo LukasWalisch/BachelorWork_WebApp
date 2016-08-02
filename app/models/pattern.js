4f6e91900ce542bf3d537cac7e239626cfea6a3a
lukas <lukas.walisch@hotmail.com>
2016-08-02 21:02:30 +0200

    - changed adapter to RESTAdapter - added inverse property to tactic model

--- app/models/pattern.js
+++ app/models/pattern.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   info: DS.attr('string'),
-  relatedPatternIds: DS.hasMany('pattern'),
-  mappingIds: DS.hasMany('mapping')
+  relatedPatternIds: DS.hasMany('pattern', {async: true}),
+  mappingIds: DS.hasMany('mapping', {async: true})
 });
